114426d603135304e6323064ebb9f9006e831190
jonatas <jonatas.brito.ferreira@gmail.com>
2025-01-09 20:53:29 -0300

    feat: add list model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_09_233544) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_09_234337) do
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "board_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_lists_on_board_id"
+  end
+
+  add_foreign_key "lists", "boards"
 end
