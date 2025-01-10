ec0c4bdeacbe4914cbe1d7a59030fb86c7bb5616
jonatas <jonatas.brito.ferreira@gmail.com>
2025-01-09 22:00:22 -0300

    feat: add task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_09_234337) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_000927) do
   create_table "boards", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -25,5 +25,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_09_234337) do
     t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.integer "priority"
+    t.boolean "done", default: false
+    t.datetime "due_date"
+    t.integer "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
   add_foreign_key "lists", "boards"
+  add_foreign_key "tasks", "lists"
 end
