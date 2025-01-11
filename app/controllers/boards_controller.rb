8a7840a14660def56ddbda2b21d1184df052ffbc
jonatas <jonatas.brito.ferreira@gmail.com>
2025-01-11 15:24:42 -0300

    feat: create board function

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,4 +7,21 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
     @lists = @board.lists
   end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to root_path, notice: "Quadro criado com sucesso!"
+    else
+      @boards = Board.all
+      flash.now[:alert] = "Erro ao criar o quadro. Verifique os campos."
+      render :index
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
 end
